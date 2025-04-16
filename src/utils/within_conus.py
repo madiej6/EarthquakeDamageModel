f144214b5136234096ce76e7902656117a6c4db1
Madie Jones <madelinejones214@gmail.com>
2025-04-15 21:20:32 -0500

    cleanup utils

--- src/utils/within_conus.py
+++ src/utils/within_conus.py
@@ -4,15 +4,13 @@ right = -66.9513812 # east long
 bottom = 24.7433195 # south lat
 
 def check_coords(lat, lng):
-    """ Accepts a list of lat/lng tuples.
-        returns the list of tuples that are within the bounding box for the US.
-        NB. THESE ARE NOT NECESSARILY WITHIN THE US BORDERS!
+    """
+    Accepts a list of lat/lng tuples.
+    
+    Returns the list of tuples that are within the bounding box for the US.
     """
     if bottom <= lat <= top and left <= lng <= right:
         inside_box = 1
     else:
         inside_box = 0
     return inside_box
-
-if __name__ == "__main__":
-    check_coords(lat, lng)
